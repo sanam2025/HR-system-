4d95647c50cb4678c17e6d2ca8ee68508f468b46
soulaf-Ramadan <ramadansoulaf401@gmail.com>
2026-05-25 13:22:46 +0300

    add icons to Employyes section

--- src/shared/layouts/HRLayout.tsx
+++ src/shared/layouts/HRLayout.tsx
@@ -1,31 +1,42 @@
 // layouts/HRLayout.tsx
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/SideBar";
-import { Users } from "lucide-react";
+import {
+  LayoutDashboard, // للـ Dashboard
+  Briefcase, // للـ Job
+  Users, // للـ Employee
+  MessageSquareWarning, // للـ Complaints
+  CalendarDays, // للـ Leaves
+  DollarSign, // للـ Payroll
+  UserPlus, // للـ Recruitment
+} from "lucide-react";
 import { useState } from "react";
 
 export default function HRLayout() {
   const [open, setOpen] = useState(true);
   const hrSideBar = [
-    { path: "/Hr", label: "Dashboard", icon: Users, exact: true },
-    { path: "Job", label: "Job", icon: Users, exact: false },
+    { path: "/Hr", label: "Dashboard", icon: LayoutDashboard, exact: true },
+    { path: "Job", label: "Job", icon: Briefcase, exact: false },
     { path: "employees", label: "Employee", icon: Users, exact: false },
-    { path: "Complaints", label: "Complaints", icon: Users, exact: false },
-    { path: "Leaves", label: "Leaves", icon: Users, exact: false },
-    { path: "Payroll", label: "Payroll", icon: Users, exact: false },
-    { path: "Recruitment", label: "Recruitment", icon: Users, exact: false },
+    {
+      path: "Complaints",
+      label: "Complaints",
+      icon: MessageSquareWarning,
+      exact: false,
+    },
+    { path: "Leaves", label: "Leaves", icon: CalendarDays, exact: false },
+    { path: "Payroll", label: "Payroll", icon: DollarSign, exact: false },
+    { path: "Recruitment", label: "Recruitment", icon: UserPlus, exact: false },
   ];
 
   return (
     <div className="flex h-screen overflow-hidden bg-gray-100">
-      {/* Sidebar on the LEFT */}
       <Sidebar
         navItems={hrSideBar}
         onToggle={() => setOpen(!open)}
         open={open}
       />
 
-      {/* Main Content - with margin left for sidebar */}
       <main
         className={`flex-1 overflow-y-auto transition-all duration-300 ${open ? "ml-64" : "ml-20"}`}
       >
