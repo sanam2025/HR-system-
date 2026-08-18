419928913220f9ed1d9c84e90bf0f03e0f08b2bb
AHMED-VIR <ahmadasa3447@gmail.com>
2026-08-18 17:40:43 +0300

    fix(api): update apiClient to use localStorage token (from AdminBranch)

---
+++ src/api/apiClient.ts
@@ -0,0 +1,66 @@
+import axios from "axios";
+
+const BASE_URL = 'http://masarhr.alwaysdata.net/api/'
+
+
+export const apiClient = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type' : 'application/json'
+    },
+    timeout: 10000
+})
+
+apiClient.interceptors.request.use(
+    (config) =>{
+        const token = localStorage.getItem('token');
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config
+    },
+    (error) =>{
+        return Promise.reject(error)
+    }
+)
+
+apiClient.interceptors.response.use(
+    (response) => {
+        // if (response.data?.data?.Token) {
+        //     localStorage.setItem('token', response.data.data.Token);
+
+        //     if (response.data.data.user) {
+        //         localStorage.setItem('user', JSON.stringify(response.data.data.user));
+        //     }
+        // }
+        
+        if (response.data?.Token) {
+            const token = response.data.Token;
+            localStorage.setItem('token', token);
+
+            if (response.data.data.user) {
+                localStorage.setItem('user', JSON.stringify(response.data.data.user));
+            }
+        }
+        
+        return response;
+    },
+    (error) => {
+        const { response } = error;
+
+        if (response) {
+            if (response.status === 401) {
+                localStorage.removeItem('token');
+                localStorage.removeItem('user');
+            }
+        }
+
+        if (response?.status === 403) {
+            console.warn("Access denied: Account pending approval.");
+        }
+
+        return Promise.reject(error);
+    }
+);
