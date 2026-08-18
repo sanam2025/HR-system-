2362ea342e497bca32113ccaea31971f6d4bead7
AHMED-VIR <ahmadasa3447@gmail.com>
2026-08-18 17:32:20 +0300

    feat(hr): update hrRoutes with Performance, Overtime, ContractDetail, ResignationDetail, offers routes

--- src/routes/hrRoutes.tsx
+++ src/routes/hrRoutes.tsx
@@ -8,8 +8,10 @@ const Recruitment = React.lazy(() => import('@/core/modules/HR/pages/Recruitment
 const Payroll = React.lazy(() => import('@/core/modules/HR/pages/Payroll'));
 const AcceptedCandidates = React.lazy(() => import('@/core/modules/HR/pages/AcceptedCandidates'));
 const Terminations = React.lazy(() => import('@/core/modules/HR/pages/Terminations'));
-const Resignations = React.lazy(() => import('@/core/modules/HR/pages/Resignations'));
-const Contracts = React.lazy(() => import('@/core/modules/HR/pages/Contracts'));
+const Resignations = React.lazy(() => import('@/core/modules/HR/pages/Resignations/Resignations'));
+const ResignationDetail = React.lazy(() => import('@/core/modules/HR/pages/Resignations/ResignationDetail'));
+const Contracts = React.lazy(() => import('@/core/modules/HR/pages/Contracts/Contracts'));
+const ContractDetail = React.lazy(() => import('@/core/modules/HR/pages/Contracts/ContractDetail'));
 const JobPostingForm = React.lazy(() => import('@/core/modules/HR/pages/JobPostings/JobPostingForm'));
 const JobPostingDetail = React.lazy(() => import('@/core/modules/HR/pages/JobPostings/JobPostingDetail'));
 const JobPostings = React.lazy(() => import('@/core/modules/HR/pages/JobPostings/JobPostings'));
@@ -18,6 +20,8 @@ const AllApplicants = React.lazy(() => import('@/core/modules/HR/pages/AllApplic
 const ScheduleInterview = React.lazy(() => import('@/core/modules/HR/pages/Interviews/ScheduleInterview'));
 const Interviews = React.lazy(() => import('@/core/modules/HR/pages/Interviews/Interviews'));
 const Attendance = React.lazy(() => import('@/core/modules/HR/pages/Attendance/Attendance'));
+const Overtime = React.lazy(() => import('@/core/modules/HR/pages/Overtime/Overtime'));
+const OvertimeDetail = React.lazy(() => import('@/core/modules/HR/pages/Overtime/OvertimeDetail'));
 const Leaves = React.lazy(() => import('@/core/modules/HR/pages/Leaves/Leaves'));
 const LeaveDetail = React.lazy(() => import('@/core/modules/HR/pages/Leaves/LeaveDetail').then(module => ({ default: module.LeaveDetail })));
 const HourlyLeaveDetail = React.lazy(() => import('@/core/modules/HR/pages/HourlyLeaves/HourlyLeaveDetail').then(module => ({ default: module.HourlyLeaveDetail })));
@@ -30,6 +34,8 @@ const Complaints = React.lazy(() => import('@/core/modules/HR/pages/Complaints/C
 const ComplaintDetail = React.lazy(() => import('@/core/modules/HR/pages/Complaints/ComplaintDetail'));
 const EmployeeProfileHR = React.lazy(() => import('@/core/modules/HR/pages/EmployeeProfile'));
 const DepartmentDetail = React.lazy(() => import('@/core/modules/HR/pages/DepartmentDetail'));
+const Performance = React.lazy(() => import('@/core/modules/HR/pages/Performance/Performance'));
+const PerformanceDetail = React.lazy(() => import('@/core/modules/HR/pages/Performance/PerformanceDetail'));
 const MyProfile = React.lazy(() => import('@/core/modules/employee/pages/EmployeeProfile'));
 
 export const hrRoutes = {
@@ -45,13 +51,16 @@ export const hrRoutes = {
     { path: "accepted-candidates", element: <SuspenseWrapper><AcceptedCandidates /></SuspenseWrapper> },
     { path: "terminations", element: <SuspenseWrapper><Terminations /></SuspenseWrapper> },
     { path: "resignations", element: <SuspenseWrapper><Resignations /></SuspenseWrapper> },
+    { path: "resignations/:id", element: <SuspenseWrapper><ResignationDetail /></SuspenseWrapper> },
     { path: "contracts", element: <SuspenseWrapper><Contracts /></SuspenseWrapper> },
+    { path: "contracts/:id", element: <SuspenseWrapper><ContractDetail /></SuspenseWrapper> },
     { path: "job-postings", element: <SuspenseWrapper><JobPostings /></SuspenseWrapper> },
     { path: "job-postings/:id", element: <SuspenseWrapper><JobPostingDetail /></SuspenseWrapper> },
     { path: "job-postings/edit/:id", element: <SuspenseWrapper><JobPostingForm /></SuspenseWrapper> },
     { path: "all-applicants", element: <SuspenseWrapper><AllApplicants /></SuspenseWrapper> },
     { path: "recruitment/applicants/:jobId", element: <SuspenseWrapper><AllApplicants /></SuspenseWrapper> },
     { path: "attendance", element: <SuspenseWrapper><Attendance /></SuspenseWrapper> },
+    { path: "overtime", element: <SuspenseWrapper><Overtime /></SuspenseWrapper> },
     { path: "Leaves", element: <SuspenseWrapper><Leaves /></SuspenseWrapper> },
     { path: "Leaves/:id", element: <SuspenseWrapper><LeaveDetail /></SuspenseWrapper> },
     { path: "recruitment/applicant/:id", element: <SuspenseWrapper><ApplicantDetail /></SuspenseWrapper> },
@@ -60,6 +69,7 @@ export const hrRoutes = {
     { path: "interviews/:id", element: <div>Interview Details - Coming Soon</div> },
     { path: "hourly-leaves", element: <SuspenseWrapper><HourlyLeaves /></SuspenseWrapper> },
     { path: "hourly-leaves/:id", element: <SuspenseWrapper><HourlyLeaveDetail /></SuspenseWrapper> },
+    { path: "offers", element: <SuspenseWrapper><Offers /></SuspenseWrapper> },
     { path: "job-postings/:jobId/offers", element: <SuspenseWrapper><Offers /></SuspenseWrapper> },
     { path: "job-postings/:jobId/offers/send", element: <SuspenseWrapper><SendOffer /></SuspenseWrapper> },
     { path: "announcements", element: <SuspenseWrapper><Announcements /></SuspenseWrapper> },
@@ -67,5 +77,8 @@ export const hrRoutes = {
     { path: "complaints/:id", element: <SuspenseWrapper><ComplaintDetail /></SuspenseWrapper> },
     { path: "employee/:id", element: <SuspenseWrapper><EmployeeProfileHR /></SuspenseWrapper> },
     { path: "department/:id", element: <SuspenseWrapper><DepartmentDetail /></SuspenseWrapper> },
+    { path: "performance", element: <SuspenseWrapper><Performance /></SuspenseWrapper> },
+    { path: "performance/:id", element: <SuspenseWrapper><PerformanceDetail /></SuspenseWrapper> },
+    { path: "overtime/:id", element: <SuspenseWrapper><OvertimeDetail /></SuspenseWrapper> },
   ],
 };
